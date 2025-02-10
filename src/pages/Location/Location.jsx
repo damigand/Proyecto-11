f1981227800965e3ed637227e0f7d49d3816fb0c
David Miguel Andrés <damigand700@gmail.com>
2025-02-10 21:58:33 +0100

    peticion de location
    Ya están los datos montados, solo falta hacer la página de location para visualizar los datos

--- src/pages/Location/Location.jsx
+++ src/pages/Location/Location.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 useParams;
 
+const url = "https://rickandmortyapi.com/api/location/";
+
+const getLocation = async (id) => {
+    const response = await fetch(`${url}${id}`);
+    const json = await response.json();
+
+    const object = {
+        name: json.name,
+        type: json.type,
+        dimension: json.dimension
+    };
+
+    let residents = "";
+
+    for (const url of json.residents) {
+        const id = url.split("character/")[1];
+        residents += id + ",";
+    }
+
+    const residentsUrl = `https://rickandmortyapi.com/api/character/${residents}`;
+    const residentsResponse = await fetch(residentsUrl);
+    const residentsJson = await residentsResponse.json();
+
+    object.residents = residentsJson.map((r) => {
+        return { image: r.image };
+    });
+
+    return json;
+};
+
 const Location = () => {
     const { id } = useParams();
+    const [location, setLocation] = useState([]);
+
+    useEffect(() => {
+        getLocation(id).then((c) => setLocation(c));
+    }, []);
+
     return <div>Location {id}</div>;
 };
 
